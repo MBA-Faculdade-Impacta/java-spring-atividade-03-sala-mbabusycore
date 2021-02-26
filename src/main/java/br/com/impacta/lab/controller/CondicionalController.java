dcf65c0467d73fe5650bcc28de82ec48f1079233
mbabusycore <matheus.vargem@aluno.faculdadeimpacta.com.br>
2021-02-26 00:16:22 +0000

    challenge done

--- src/main/java/br/com/impacta/lab/controller/CondicionalController.java
+++ src/main/java/br/com/impacta/lab/controller/CondicionalController.java
@@ -24,9 +24,11 @@ public class CondicionalController {
 		 *  Para menores de dezoito anos -> Possui menos de 18 anos
 		 *  
 		 */
+
+
 		
 		
-		return ResponseEntity.ok("APÓS O ALGORITMO COLOCAR O TEXTO/VARIAVEL DE RETORNO AQUI");
+		return ResponseEntity.ok(idade>=18 ? "Possui mais de 18 anos" : "Possui menos de 18 anos");
 	}
 	
 }
